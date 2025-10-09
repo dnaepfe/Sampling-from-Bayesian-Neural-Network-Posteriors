54522fdc509b94c42a6f509fc8e8ff1e4f02a9ee
dnaepfe <dominicnaepfer@gmail.com>
2025-10-09 22:07:17 +0200

    first

---
+++ Stochastic Gradient MCMC Algorithms/Approximate vs. Exact MCMC/Comparison ULA and MALA.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+def ULA(theta_0,step_size,num_samples,grad_log):
+    
+    theta = np.array(theta_0, dtype=float).reshape(-1)
+    d = x.size
+    samples = np.emtpy((num_samples,d))
+    samples[0] = theta
+
+    for k in range(0,num_samples-1):
+        
+
+
+    return samples
+
+def MALA():
